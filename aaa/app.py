04271c131945624e682e9cb05a4967af3700b6ee
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2022-01-05 15:54:48 +0100

    front page update with input data

--- aaa/app.py
+++ aaa/app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-data = (
-        ('samochod', 1230),
-        ('mieszkanie', 1990),
-        ('jedzenie', 600),
-    )
+data = [
+        ['samochod', 1230],
+        ['mieszkanie', 1990],
+        ['jedzenie', 600],
+]
 
 
 money = 3240 
@@ -19,10 +19,21 @@ def index_base():
 
 @app.route('/', methods=['POST'])
 def my_money():
-    moneey = request.form['moneyy']
     global money
-    money += int(moneey)
-    return render_template('index_base.html',expenditures=data, money=money)
+    global data
+    if request.form['btn'] == 'Dodaj':
+        moneey = request.form['money']
+        money += int(moneey)
+        return render_template('index_base.html',expenditures=data, money=money)
+    elif request.form['btn'] == 'zatwierdź':
+        expenditure = request.form['expenditure']
+        cost = request.form['cost']
+        data.append([expenditure, cost])
+        return render_template('index_base.html',expenditures=data, money=money)
+        
+    
+
+
 
 
 
